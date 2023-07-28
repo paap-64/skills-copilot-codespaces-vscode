7c3605a2b46b27db7d051e683b0059a9c645dc69
paap-64 <134977179+paap-64@users.noreply.github.com>
2023-07-28 19:40:36 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,79 @@
+// Create web server
+var express = require('express');
+var app = express();
+// Create a route
+app.get('/', function(req, res) {
+  // Send a response to the client
+  res.send('Hello World!');
+});
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+// Path: index.js
+// Create web server
+var express = require('express');
+var app = express();
+// Create a route
+app.get('/', function(req, res) {
+  // Send a response to the client
+  res.send('Hello World!');
+});
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+// Path: server.js
+// Create web server
+var express = require('express');
+var app = express();
+// Create a route
+app.get('/', function(req, res) {
+  // Send a response to the client
+  res.send('Hello World!');
+});
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+// Path: skills.js
+// Create web server
+var express = require('express');
+var app = express();
+// Create a route
+app.get('/', function(req, res) {
+  // Send a response to the client
+  res.send('Hello World!');
+});
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+// Path: skills.js
+// Create web server
+var express = require('express');
+var app = express();
+// Create a route
+app.get('/', function(req, res) {
+  // Send a response to the client
+  res.send('Hello World!');
+});
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+// Path: skills.js
+// Create web server
+var express = require('express');
+var app = express();
+// Create a route
+app.get('/', function(req, res) {
+  // Send a response to the client
+  res.send('Hello World!');
+});
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+// Path: skills.js
+
